69ba46c50b3ec001a18f0702190a9e43ae3aa647
Radek <roszko.radek@gmail.com>
2019-09-07 12:44:39 +0200

    initial commit

---
+++ src/main/java/pl/sda/grocefy/GrocefyApplication.java
@@ -0,0 +1,13 @@
+package pl.sda.grocefy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrocefyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrocefyApplication.class, args);
+    }
+
+}
