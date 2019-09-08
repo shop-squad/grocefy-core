5e601a1d043edfa4eda7f320130b8c5eb1f88e5c
Radek <roszko.radek@gmail.com>
2019-09-08 13:59:17 +0200

    Enum z miarami dodany do ModelAndView

--- src/main/java/pl/sda/grocefy/product/controller/HomeController.java
+++ src/main/java/pl/sda/grocefy/product/controller/HomeController.java
@@ -3,6 +3,7 @@ package pl.sda.grocefy.product.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import pl.sda.grocefy.product.entity.Unit;
 
 import java.time.LocalDateTime;
 
@@ -13,6 +14,7 @@ public class HomeController {
     public ModelAndView getHomePage(){
         ModelAndView mav = new ModelAndView("index");
         mav.addObject("hash", generateHash());
+        mav.addObject("units", Unit.values());
         return mav;
     }
 
